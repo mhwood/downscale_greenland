d09f750868afef7bb26ff49bf548f389f8fa978f
Michael Wood <mhwood@uci.edu>
2024-07-25 15:48:31 -0700

    pushing model config files for L2_Kanger

---
+++ L2/L2_Kanger/code_melange/DIAGNOSTICS_VEC_SIZE.h
@@ -0,0 +1,7 @@
+C------------------------------------------------------------------------------|
+C                           DIAGNOSTICS_VEC_SIZE.h
+C------------------------------------------------------------------------------|
+
+      INTEGER, PARAMETER :: VEC_points = 400
+      INTEGER, PARAMETER :: nVEC_mask = 3
+      INTEGER, PARAMETER :: nSURF_mask = 1
